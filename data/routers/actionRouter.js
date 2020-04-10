50e364ad36bf9995d509e8cea1e26ff0d3ff4ced
JGPico <minq_2@hotmail.com>
2020-04-10 11:14:12 -0600

    Almost there

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -2,112 +2,98 @@ const express = require('express');
 
 const router = express.Router();
 const Actions = require('../helpers/actionModel.js');
+const Projects = require('../helpers/projectModel.js');
 
-// router.post('/', validateUser, (req, res) => {
-//     Users.insert(req.body)
-//     .then(user => {
-//       res.status(201).json(user)
-//     })
-//     .catch(err => {
-//       res.status(500).json({error: "Error adding user"});
-//     })
-//   });
+router.post('/', validateAction, (req, res) => {
+    Actions.insert(req.body)
+    .then(act => {
+      res.status(201).json(act)
+    })
+    .catch(err => {
+      res.status(500).json({error: "Error adding action"});
+    })
+  });
   
-//   router.post('/:id/posts', validatePost, (req, res) => {
-//     //console.log(req.body);
-//     Users.insert(req.body)
-//     .then(post => {
-//       res.status(201).json(post);
-//     })
-//     .catch(err => {
-//       res.status(500),json({error: "Error adding post"});
-//     })
-//   });
   
-//   router.get('/', (req, res) => {
-//     Users.get()
-//     .then(userArray => {
-//       res.status(200).json(userArray);
-//     })
-//     .catch(err => {
-//       res.status(500).json({error: "Failed to get users"});
-//     });
+  router.get('/', (req, res) => {
+    Actions.get()
+    .then(actArray => {
+      res.status(200).json(actArray);
+    })
+    .catch(err => {
+      res.status(500).json({error: "Failed to get actions"});
+    });
   
-//   });
+  });
   
-//   router.get('/:id', validateUserId, (req, res) => {
-//     Users.getById(req.params.id)
-//     .then(user => {
-//       res.status(200).json(user);
-//     })
-//     .catch(err => {
-//       res.status(500).json({error: "Failed to get user"})
-//     })
-//   });
+  router.get('/:id', validateActionId, (req, res) => {
+    Actions.get(req.params.id)
+    .then(proj => {
+      res.status(200).json(proj);
+    })
+    .catch(err => {
+      res.status(500).json({error: "Failed to get project by id"})
+    })
+  });
   
-//   router.get('/:id/posts', validateUserId, (req, res) => {
-//     Users.getUserPosts(req.params.id)
-//     .then(posts => {
-//       res.status(200).json(posts);
-//     })
-//     .catch(err => {
-//       res.status(500).json({error: "Failed to get user posts"});
-//     })
-//   });
   
-//   router.delete('/:id', validateUserId, (req, res) => {
-//     Users.remove(req.params.id)
-//     .then(user => {
-//       res.status(200).json({message: "Successfully deleted user"})
-//     })
-//   });
+  router.delete('/:id', validateActionId, (req, res) => {
+    Actions.remove(req.params.id)
+    .then(proj => {
+      res.status(200).json({message: "Successfully deleted action"})
+    })
+  });
   
-//   router.put('/:id', validateUserId, validateUser, (req, res) => {
-//     Users.update(req.params.id, req.body)
-//     .then(user => {
-//       res.status(200).json({message: "Successfully updated user"});
-//     })
-//     .catch(err => {
-//       res.status(500).json({error: "Error editing user"});
-//     })
-//   });
+  router.put('/:id', validateActionId, validateAction, (req, res) => {
+    Actions.update(req.params.id, req.body)
+    .then(act => {
+      res.status(200).json({message: "Successfully updated action"});
+    })
+    .catch(err => {
+      res.status(500).json({error: "Error editing action"});
+    })
+  });
   
   //custom middleware
   
-//   function validateUserId(req, res, next) {
-//     Users.getById(req.params.id)
-//     .then(userBit => {
-//       if (userBit) {
-//         req.user = userBit;
-//         next();
-//       } else {
-//         res.status(400).json({error: "invalid user id"});
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).json({error: "Error fetching user"});
-//     })
+  function validateActionId(req, res, next) {
+    Actions.get(req.params.id)
+    .then(actBit => {
+      if (actBit) {
+        next();
+      } else {
+        res.status(400).json({error: "invalid action id"});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({error: "Error fetching action"});
+    })
     
-//   }
+  }
   
-//   function validateUser(req, res, next) {
-//     if (!req.body) {
-//       res.status(400).json({error: "Missing user data"});
-//     } else if (!req.body.name) {
-//       res.status(400).json({error: "Missing required name field"});
-//     } else {
-//       next();
-//     }
-//   }
-  
-//   function validatePost(req, res, next) {
-//     if (!req.body) {
-//       res.status(400).json({error: "Missing post data"});
-//     } else if (!req.body.text) {
-//       res.status(400).json({error: "Missing required text field"});
-//     } else {
-//       next();
-//     }
-//   }
+  function validateAction(req, res, next) {
+
+    Projects.get()
+    .then(projArr => {
+        let projectArray = projArr;
+        let idArray = projectArray.map(el => el.id);
+        let idBool = idArray.find(req.body.project_id);
+        
+
+        if (!req.body) {
+            res.status(400).json({error: "Missing action data"});
+          } else if (!req.body.project_id || !req.body.description || !req.body.notes) {
+            res.status(400).json({error: "Missing required data fields"});
+          } else if (!idBool) {
+              res.status(400).json({error: "project id doesn't match existing project"});
+          } else {
+            next(); 
+          }
+
+    })
+    .catch(err => {
+        res.status(500).json({error: "Internal server error"});
+    });
+  }
 
 module.exports = router;
